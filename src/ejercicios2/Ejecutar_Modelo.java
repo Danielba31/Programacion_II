ee2e08c9646d165545156890950b1874a92d70ca
Daniel <danieldicelis@gmail.com>
2024-08-28 08:14:17 -0500

    comentarios de las clases nuevas

--- src/ejercicios2/Ejecutar_Modelo.java
+++ src/ejercicios2/Ejecutar_Modelo.java
@@ -10,24 +10,24 @@ package ejercicios2;
  */
 import java.util.Arrays;
 import java.util.Scanner;
-public class Ejecutar_Modelo {
+public class Ejecutar_Modelo {//clase principal
     public static void main(String[]args){
-    Scanner input = new Scanner(System.in);
-    Modelos_Evaluacion obj = new Modelos_Evaluacion();
+    Scanner input = new Scanner(System.in);//creamos un objeto tipo scanner 
+    Modelos_Evaluacion obj = new Modelos_Evaluacion();//creamos el objeto de nuestra otra clase 
     
-        System.out.println("digite cuantos estudiantes va a ingresar: ");
+        System.out.println("digite cuantos estudiantes va a ingresar: ");//le pedimos al usuario la cantidad de estudiantes 
         int cantidad_e = input.nextInt();
         
-        int []llenar = new int[cantidad_e];
-        System.out.println("ingrese el ultimo numero de documento de cada estudiante: ");
-        for(int i=0;i<llenar.length;i++){
+        int []llenar = new int[cantidad_e];//se declara una array con el tamaño de la cantidad de estudiantes 
+        System.out.println("ingrese el ultimo numero de documento de cada estudiante: ");//le pedimos al usuario que digite el ultimo digito de su documento 
+        for(int i=0;i<llenar.length;i++){//funcion for que se encarga de leer y guardar cada numero 
         llenar[i]= input.nextInt();
         }
-        obj.U_Digito(llenar);
-        obj.Comparar_Digito();
-        for(int i =1;i<=cantidad_e;i++){
+        obj.U_Digito(llenar);//llamamos al objeto que habiamos creado con la funcion U_Digito y le asignamos la array llenar 
+        obj.Comparar_Digito();//llamamos un objeto con la funcion comparar digito para que haga las operaciones correspondientes 
+        for(int i =1;i<=cantidad_e;i++){//for que se encarga de mostrar los estudiantes con su numero de documento y el tipo de modelo que deben presentar 
            
-        System.out.println("el modelo del estudiante: "+i+ " con numero de: "+llenar[i-1]+" es: "+ obj.getTipo_Modelo(i-1));
+        System.out.println("el modelo del estudiante: "+i+ " con numero de documento: "+llenar[i-1]+" es: "+ obj.getTipo_Modelo(i-1));
     
     }
     }
--- src/ejercicios2/Modelos_Evaluacion.java
+++ src/ejercicios2/Modelos_Evaluacion.java
@@ -8,18 +8,18 @@ package ejercicios2;
  *
  * @author Daniel Barrios
  */
-public class Modelos_Evaluacion {
-    int []U_Digito= new int[0];
+public class Modelos_Evaluacion {//declaramos 2 arrays vacias las cuales se encargaran de llenar y mostrar los resultados y una llena
+    int []U_Digito= new int[0];  //para que podamos hacer la condicional que dependiendo del numero se le muestre un espacio de la array modelo 
     String []Modelo= {"Modelo_1","Modelo_2","Modelo_3","Modelo_4","Modelo_5"};
     String []Tipo_Modelo;
     
-    public void U_Digito(int []U_Digito){
+    public void U_Digito(int []U_Digito){//metodo constructor que se encarga de llenar la array 
     this.U_Digito=U_Digito;
-    this.Tipo_Modelo=new String[U_Digito.length];
+    this.Tipo_Modelo=new String[U_Digito.length];//le damos el mismo tamaño de nuestsra primera array a esta 
     }
     
-    public void Comparar_Digito(){
-    for(int i=0; i<U_Digito.length;i++){
+    public void Comparar_Digito(){// se comparan las variables de ultimos digitos para saber que tipo de modelo es para cada estudiante
+    for(int i=0; i<U_Digito.length;i++){//funcion for para las codicionales y el almacenamiento de los resultados 
        
     if(U_Digito[i] == 1 || U_Digito[i] == 6){
        Tipo_Modelo[i]=Modelo[0];
@@ -35,11 +35,11 @@ public class Modelos_Evaluacion {
    }
   }
 
-    public String getTipo_Modelo(int i) {
+    public String getTipo_Modelo(int i) {//retornamos las variable de tipo modelo 
         return Tipo_Modelo[i];
     }
 
-    public int[] getU_Digito() {
+    public int[] getU_Digito() {//retornamos la variable de ultimo digito
         return U_Digito;
     }
     
